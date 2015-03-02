44a8848efc34475473ad6a762cd2dbce141441c5
chamaoskurumi <gosz@gmx.de>
2015-03-02 20:11:36 +0100

    new

--- 4_GentriDataExplorer_shiny.R
+++ 4_GentriDataExplorer_shiny.R
@@ -16,96 +16,8 @@ GentriMotionChartUI <- shinyUI(pageWithSidebar(
     checkboxGroupInput("AusgewaehlteBezirke","Bezirke:",
                        choices = levels(LORdata4ggvis$BEZ_NAME),
                        selected = "Friedrichshain-Kreuzberg"),
-    submitButton(text="Update!")),
-  mainPanel(htmlOutput("LORggvis"))
-  )
-)
-
-GentriMotionChartSERVER <- shinyServer(function (input, output, session) {
-  LORdata4ggvisSUBSET <- reactive({
-    b <- subset(LORdata4ggvis, BEZ_NAME %in% input$AusgewaehlteBezirke)
-    b <- droplevels(b)
-    return(b)
-  })
-  output$LORggvis <- renderGvis({
-      newData <- LORdata4ggvisSUBSET()
-      LORggvis <- gvisMotionChart(newData, 
-                     idvar   = "RAUMID_NAME",
-                     timevar = "ZEIT",
-                     sizevar = "E_E",
-                     colorvar= "BEZ_NAME",
-                     options=list(width=700, height=400))
-     return(LORggvis)
-    })
-})
-
-GentriMotionChart <- runApp(
-  list(server = GentriMotionChartSERVER, 
-       ui     = GentriMotionChartUI))
-GentriMotionChart
-
-
-###################
-
-library(googleVis)
-library(shiny)
-names(LORdataFULL)
-LORdata4ggvis <- LORdataFULL
-LORdata4ggvis$ZEIT <- as.numeric(as.character(LORdata4ggvis$ZEIT))
-
-GentriMotionChartUI <- shinyUI(pageWithSidebar(
-  headerPanel("Gentri-MotionChart"),
-  sidebarPanel(
-    checkboxGroupInput("AusgewaehlteBezirke","Bezirke:",
-                       choices = levels(LORdata4ggvis$BEZ_NAME),
-                       selected = "Friedrichshain-Kreuzberg"),
-    submitButton(text="Update!")),
-  mainPanel(tabsetPanel(
-    tabPanel("Motion Chart", htmlOutput("LORggvis")),
-    tabPanel("Daten", dataTableOutput("LORggvisDaten"))
-))))
-
-GentriMotionChartSERVER <- shinyServer(function (input, output, session) {
-  LORdata4ggvisSUBSET <- reactive({
-    b <- subset(LORdata4ggvis, BEZ_NAME %in% input$AusgewaehlteBezirke)
-    b <- droplevels(b)
-    return(b)
-  })
-  output$LORggvis <- renderGvis({
-    newData <- LORdata4ggvisSUBSET()
-    LORggvis <- gvisMotionChart(newData, 
-                                idvar   = "RAUMID_NAME",
-                                timevar = "ZEIT",
-                                sizevar = "E_E",
-                                colorvar= "BEZ_NAME",
-                                options=list(width=700, height=400))
-    return(LORggvis)
-  })
-  output$LORggvisDaten <- renderDataTable({LORdata4ggvisSUBSET},
-                                          options = list(lengthMenu = c(10, 50, 100), pageLength = 50))  
-})
-
-GentriMotionChart <- runApp(
-  list(server = GentriMotionChartSERVER, 
-       ui     = GentriMotionChartUI))
-GentriMotionChart
-
-#######################
-
-
-library(googleVis)
-library(shiny)
-names(LORdataFULL)
-LORdata4ggvis <- LORdataFULL
-LORdata4ggvis$ZEIT <- as.numeric(as.character(LORdata4ggvis$ZEIT))
-
-GentriMotionChartUI <- shinyUI(pageWithSidebar(
-  headerPanel("Gentri-MotionChart"),
-  sidebarPanel(
-    checkboxGroupInput("AusgewaehlteBezirke","Bezirke:",
-                       choices = levels(LORdata4ggvis$BEZ_NAME),
-                       selected = "Friedrichshain-Kreuzberg"),
-    submitButton(text="Update!")),
+    submitButton(text="Update!"),
+    width = 3),
   mainPanel(tabsetPanel(
     tabPanel("Motion Chart", helpText("blabla")),
     tabPanel("Daten", dataTableOutput("LORggvisDaten"))
@@ -154,10 +66,9 @@ GentriMotionChartUI <- shinyUI(navbarPage("GentriData Explorer",
                                   checkboxGroupInput("VarSelection","Variablen:",
                                                      choices = names(LORdata4ggvis),
                                                      selected = c("ZEIT",
-                                                                  "RAUMID_NAME",
-                                                                  "BEZ_NAME")),
+                                                                  "RAUMID_NAME")),
                                 submitButton(text="Update!")),
-                                mainPanel("LOR Planungsräume von Berlin Rohdaten", 
+                                mainPanel("Rohdaten", 
                                           dataTableOutput("LORggvisDaten"))
                      )
                      )
@@ -176,7 +87,8 @@ GentriMotionChartSERVER <- shinyServer(function (input, output, session) {
                                 timevar = "ZEIT",
                                 sizevar = "E_E",
                                 colorvar= "BEZ_NAME",
-                                options=list(width=700, height=400))
+                                options=list(width=800, 
+                                             height=500))
     return(LORggvis)
   })
   output$LORggvisDaten <- renderDataTable({
