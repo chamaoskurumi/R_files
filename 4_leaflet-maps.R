4c0cff05a9de6dba3d727d56026b84130a259cd1
chamaoskurumi <gosz@gmx.de>
2015-03-16 19:54:38 +0100

    new

--- 4_leaflet-maps.R
+++ 4_leaflet-maps.R
@@ -22,17 +22,17 @@ LOR4leaflet <- LOR
 LOR4leaflet@data <- subset(LOR4leaflet@data, select=c(RAUMID, RAUMID_NAME,BZR,BZR_NAME,
                                                       PGR,PRG_NAME,BEZ,BEZ_NAME,                   
                                                       STADTRAUM,FL_HA,
-                                                      Miete.2013))
+                                                      Miete.2012))
 LORjson <- toGeoJSON(data=LOR4leaflet, dest=tempdir())
-brksIntervalls <- classIntervals(LOR4leaflet@data$Miete.2013, n=10); brksIntervalls
+brksIntervalls <- classIntervals(LOR4leaflet@data$Miete.2012, n=10); brksIntervalls
 brks           <- round(brksIntervalls$brks, digits=1); brks
-#brks <- seq(3, max(LOR@data$EWdichte2013, na.rm=T), by=1000); length(brks)
+#brks <- seq(3, max(LOR@data$EWdichte2012, na.rm=T), by=1000); length(brks)
 clrs <- colorRampPalette(c("yellow", "red"))(length(brks))
-stl <- styleGrad(prop="Miete.2013", breaks=brks, style.val=clrs, 
-                 out=1, leg="Median Angebotsmiete 2013", 
+stl <- styleGrad(prop="Miete.2012", breaks=brks, style.val=clrs, 
+                 out=1, leg="Median Angebotsmiete 2012", 
                  lwd=1, col="white", alpha=0.4)
 SPleaflet  <- leaflet(data=LORjson, dest=tempdir(),
-                      title="Median Angebotsmiete 2013", base.map="darkmatter",
+                      title="Median Angebotsmiete 2012", base.map="darkmatter",
                       style=stl, popup="*")
 SPleaflet
 #View(LOR@data)
@@ -40,29 +40,29 @@ names(LOR@data)
 
 
 myleaflet(SPdata.frame = LOR,
-          layer = "Miete.2008",
+          layer = "Miete.2007",
           popupNAMES = c("RAUMID", "RAUMID_NAME",
                          "BZR_NAME",
-                         "Miete_H2_wmean.2008"), 
+                         "Miete.2007"), 
           base.mapNAME = "positron", 
           colorNAMES = c("yellow", "red"),
           roundDIGITS = 1, 
           intervallBRKno = 10,
-          titleNAME = "Miete pro LOR 2008")
+          titleNAME = "Miete pro LOR 2007")
 
 myleaflet(SPdata.frame = LOR,
-          layer = "Miete_H2_wmean.2013",
+          layer = "Miete.2012",
           popupNAMES = c("RAUMID", "RAUMID_NAME",
                          "BZR_NAME",
-                         "Miete_H2_wmean.2008"), 
+                         "Miete.2012"), 
           base.mapNAME = "positron", 
           colorNAMES = c("yellow", "red"),
           roundDIGITS = 1, 
           intervallBRKno = 10,
-          titleNAME = "Miete pro PLZ 2.Hjahr 2013")
+          titleNAME = "Miete pro LOR 2012")
 
 
-myleaflet(SPdata.frame = PLZ2013  ,
+myleaflet(SPdata.frame = PLZ2013,
           layer = "Miete_H2",
           popupNAMES = "*", 
           base.mapNAME = "tls", 
@@ -108,7 +108,8 @@ library(plotGoogleMaps)
 library(RColorBrewer)
 
 LOR4leaflet <- LOR
-m<-plotGoogleMaps(LOR4leaflet,zcol="Miete_H2_wmean.2013",
+names(LOR4leaflet)
+m<-plotGoogleMaps(LOR4leaflet,zcol="Miete.2012",
                   filename=,
                   mapTypeId='TERRAIN',
                   colPalette= colorRampPalette(c("yellow", "red"))(10),
