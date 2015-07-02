bc503aa21be959668e89a361ff9f799f4d0a5050
chamaoskurumi <gosz@gmx.de>
2015-07-02 12:34:39 +0200

    new

--- 5_Reg_Binnenwanderungen.R
+++ 5_Reg_Binnenwanderungen.R
@@ -6,6 +6,10 @@
 #*************************************************
 #*************************************************
 
+setwd("/home/dao/Desktop/MasterArbeit/R_files/KNITR/")
+load(file = "FULL_FINAL_WORKSPACE.Rdata")
+
+
 # ____ Packages ______ ----------------------------------------------------------------
 
 library("rgdal")
@@ -39,6 +43,7 @@ LOR4reg@data$ArmutchgC               <- scale(LOR4reg@data$Armutchg,scale=F)
 LOR4reg@data$Armut.2007C             <- scale(LOR4reg@data$Armut.2007,scale=F)
 LOR4reg@data$AlosechgC               <- scale(LOR4reg@data$Alosechg,scale=F)
 LOR4reg@data$Alose.2007C             <- scale(LOR4reg@data$Alose.2007,scale=F)
+LOR4reg@data$PDAU5.2007C             <- scale(LOR4reg@data$PDAU5.2007,scale=F)
 LOR4reg@data$PDAU10.2007C            <- scale(LOR4reg@data$PDAU10.2007,scale=F)
 LOR4reg@data$E_U18R.2007C            <- scale(LOR4reg@data$E_U18R.2007,scale=F)
 LOR4reg@data$E_18U35R.2007C          <- scale(LOR4reg@data$E_18U35R.2007,scale=F)
@@ -204,19 +209,29 @@ moran.test(LOR4reg@data$FortzuegeR, listw = W_polyIDWs, na.action=na.omit)
 # IV.) VIF step um Multicollinerarity innerhalb der Predictors auszuschließen ------
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-# PDAU5.2012 und PDAU10.2012 geht nicht --> nur PDAU10.2012
-# Alose_langzeit und Armut.2012 geht nicht --> Armut.2012
-# Altersarmut und Armut.2012 geht nicht --> Armut.2012
+# PDAU5.2007C und PDAU10.2007C geht nicht --> nur PDAU10.2007C
+# Alose_langzeit und Armut.2007C geht nicht --> Armut.2007C
+# Altersarmut und Armut.2007C geht nicht --> Armut.2007C
 # --> alle Alose/Armutschg raus ausser --> Armutchg
-# Miete.2012 und Mietechgr muss geklärt werden
+# Miete.2007 und Mietechgr muss geklärt werden
 
 predictors4vifcheck <- subset(LOR4reg@data, 
-                              select=c(Mietechgr, 
-                                       Armutchg,
-                                       PDAU5chg, nicht_Alose_Hartzchg , Alose_langzeitchg , Alose_u25chg ,
-                                       AlleinerzHH.2012 , Altersarmut.2012 , StaedtWohnungen.2012 , 
-                                       PDAU5.2012 , Miete.2012 , Alose_langzeit.2012, 
-                                       PDAU10.2012 , E_U18R.2007 , E_65U110R.2007, Armut.2012))
+                              select=c(MietechgrC, Miete.2007C,
+                                       ArmutchgC, Armut.2007C,
+                                       nicht_Alose_Hartzchg, Alose_langzeitchg , Alose_u25chg ,
+                                       Altersarmut.2012,  Alose_langzeit.2012, 
+                                       StaedtWohnungen.2012C, AlleinerzHH.2012 , 
+                                       PDAU5.2007C, PDAU10.2007C, PDAU5chg, 
+                                       E_U18R.2007C, E_18U35R.2007C, E_65U110R.2007C,
+                                       dist2STADTMITTE))
+
+predictors4vifcheck <- subset(LOR4reg@data, 
+                              select=c(MietechgrC, Miete.2007C,
+                                       ArmutchgC, Armut.2007C,
+                                       StaedtWohnungen.2012C, AlleinerzHH.2012 , 
+                                       PDAU10.2007C, PDAU5chg, 
+                                       E_U18R.2007C, E_18U35R.2007C, E_65U110R.2007C,
+                                       dist2STADTMITTE))                                                           
 
 predictors4vifcheck <- subset(LOR4reg@data,
                               select=c(Mietechgr,Alosechg,
@@ -229,10 +244,15 @@ corrgram(predictors4vifcheck, order=FALSE, lower.panel=panel.shade,
          main="Correlations") 
 
 str(predictors4vifcheck)
-vifstep1 <- vifstep(predictors4vifcheck, th=5)
+vifstep1 <- vifstep(predictors4vifcheck, th=5) ; vifstep1
+vifstep2 <- vifstep(predictors4vifcheck, th=10); vifstep2
+
+vifcor1 <- vifcor(predictors4vifcheck, th=0.7) ; vifcor1
+vifcor2 <- vifcor(predictors4vifcheck, th=0.7); vifcor2
 
 vifcor(predictors4vifcheck, th=5)# identify collinear variables that should be excluded
 predictors <- exclude(predictors4vifcheck, vifstep1)
+predictors <- exclude(predictors4vifcheck, vifstep2)
 names(predictors)
 
 #Mietechgr, Armutchg, STADTRAUM, Sanierung
@@ -245,6 +265,13 @@ lm1 <- lm(formula=FortzuegeR ~ AlosechgC + MietechgrC +   Miete.2007C +
           data=LOR4reg@data,
           weights=E_E.2012)
 summary(lm1)
+
+lm2 <- lm(formula=FortzuegeR ~ MietechgrC+ Miete.2007C+ ArmutchgC+ Armut.2007C+ StaedtWohnungen.2012C+ AlleinerzHH.2012C + 
+PDAU10.2007C+ E_U18R.2007C+ E_18U35R.2007C+ E_65U110R.2007C+ #dist2STADTMITTE 
+  + STADTRAUM + SanGebiet.2007,
+data=LOR4reg@data,
+weights=E_E.2007)
+summary(lm2)
 
 weight <- qsec/sum(qsec)
 ggpairs(mtcars[ ,c("mpg", "wt", "disp", "qsec")], columns = 1:3, size = "qsec", weight="qsec",
@@ -325,29 +352,36 @@ SAR4 <- spautolm(formula=FortzuegeR ~ Gentri*STADTRAUM +
                  family="SAR")
 summary(SAR4, Nagelkerke=T)#, adj.se=T)
 
-SAR5 <- spautolm(formula=FortzuegeR ~ Mietechgr + ArmutchgC +
-                   Miete.2007C  + Armut.2007C +
-                   E_U18R.2007C +
-                   E_18U35R.2007C  +
-                   #E_65U110R.2007 +
-                   PDAU10.2007C + STADTRAUM + StaedtWohnungen.2012C + AlleinerzHH.2012C,
+SAR5 <- spautolm(formula=FortzuegeR ~ MietechgrC + Miete.2007C+ ArmutchgC + Armut.2007C + StaedtWohnungen.2012C+ AlleinerzHH.2012C + 
+                   PDAU10.2007C+ E_U18R.2007C+ E_18U35R.2007C+ E_65U110R.2007C+ STADTRAUM + SanGebiet.2007,
                  data=LOR4reg,
                  listw=W_2000mIDWs, 
                  weights=E_E.2007,
                  family="SAR")
-summary(SAR5, Nagelkerke=T)#, adj.se=T)
+summary(SAR5, Nagelkerke=T)
+qqnorm(SAR5$fit$residuals/(SAR5$weights^2)); qqline(SAR5$fit$residuals/(SAR5$weights^2), col = 2)
+qqnorm(SAR5$fit$signal_stochastic); qqline(SAR5$fit$signal_stochastic, col = 2)
 
-SAR5 <- spautolm(formula=ZuzuegeDAR ~ Mietechgr + ArmutchgC +
-                   Miete.2007C  + Armut.2007C +
-                   E_U18R.2007C +
-                   E_18U35R.2007C  +
-                   #E_65U110R.2007 +
-                   PDAU10.2007C + STADTRAUM + StaedtWohnungen.2012C + AlleinerzHH.2012C,
+SAR6 <- spautolm(formula=ZuzuegeUDAR ~ MietechgrC+ Miete.2007C+ ArmutchgC+ Armut.2007C+ StaedtWohnungen.2012C+ AlleinerzHH.2012C + 
+                   PDAU10.2007C+ E_U18R.2007C+ E_18U35R.2007C+ E_65U110R.2007C+ STADTRAUM + SanGebiet.2007,
+                 data=LOR4reg,
+                 listw=W_2000mIDWs, 
+                 weights=E_E.2007,
+                 family="SAR")
+summary(SAR6, Nagelkerke=T)
+qqnorm(SAR6$fit$residuals); qqline(SAR6$fit$residuals, col = 2)
+qqnorm(SAR6$fit$residuals/(SAR6$weights^2)); qqline(SAR6$fit$residuals/(SAR6$weights^2), col = 2)
+qqnorm(SAR6$fit$signal_stochastic); qqline(SAR6$fit$signal_stochastic, col = 2)
+
+SAR5 <- spautolm(formula=ZuzuegeDAR ~ MietechgrC+ Miete.2007C+ ArmutchgC*STADTRAUM+ Armut.2007C+ StaedtWohnungen.2012C+ AlleinerzHH.2012C + 
+                   PDAU10.2007C+ E_U18R.2007C+ E_18U35R.2007C+ E_65U110R.2007C+ STADTRAUM + SanGebiet.2007,
                  data=LOR4reg,
                  listw=W_2000mIDWs, 
                  weights=E_E.2007,
                  family="SAR")
 summary(SAR5, Nagelkerke=T)#, adj.se=T)
+qqnorm(SAR5$fit$residuals/(SAR5$weights^2)); qqline(SAR5$fit$residuals/(SAR5$weights^2), col = 2)
+qqnorm(SAR5$fit$signal_stochastic); qqline(SAR5$fit$signal_stochastic, col = 2)
 
 
 SAR5 <- spautolm(formula=FortzuegeR ~ Mietechgr*ArmutchgCAT2 +
